6d586f6dd1e5a86a41bbea28fe38dff5f6077964
rlahfld54 <rlahfld54@naver.com>
2021-09-27 18:32:03 +0900

    수정 중

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+      //안내창
+      let modal = $('.modal');
+      let modal_box = $('.modal-box');
+      let modal_close = $('.madal-close');
+  
+      modal.click(function () {
+          $(this).fadeOut();
+      });
+});
