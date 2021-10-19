5486c5b5acca9e99ad4f16d02f9c784db888a184
rlahfld54 <rlahfld54@naver.com>
2021-10-19 20:59:45 +0900

    최종업로드

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 $(document).ready(function () {
-      //안내창
-      let modal = $('.modal');
-      let modal_box = $('.modal-box');
-      let modal_close = $('.madal-close');
-  
-      modal.click(function () {
-          $(this).fadeOut();
-      });
+  //안내창
+  let modal = $(".modal");
+  let modal_con = $(".modal-con");
+  let modal_close = $(".madal-close");
+
+  modal.click(function () {
+    $(this).fadeOut();
+  });
+
+  modal_close.click(function () {
+    $(this).hide();
+    console.log("닫기버튼");
+  });
 });
